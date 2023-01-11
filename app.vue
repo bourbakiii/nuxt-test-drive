fe998d33160701db3d412a78548a424c01d1c60c
bourbaki <bourbaki@MacBook-Air-bourbaki.local>
2023-01-11 23:47:35 +0300

    Bootstrap installed

--- app.vue
+++ app.vue
@@ -1,5 +1,11 @@
 <template>
   <NuxtLayout/>
+  <div class="btn-group">
+    <!-- <NuxtPage /> -->
+    <div class="btn btn-success">Success</div>
+    <div class="btn btn-danger">Danger</div>
+    <div class="btn btn-warning">Warning</div>
+  </div>
 </template>
 
 <script setup>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@
 export default defineNuxtConfig({
     runtimeConfig: {
         API_URL: "Some api URL right there"
-    }
-})
+    },
+    css: ["bootstrap/dist/css/bootstrap.min.css"], // add
+    vite: {
+        define: {
+            "process.env.DEBUG": false,
+        },
+    },
+});
